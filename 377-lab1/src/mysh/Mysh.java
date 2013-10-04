002c6ac82c99c337c5daa6eed20d6dc07e0925b1
nh0815 <nh0815@gmail.com>
2013-10-03 20:01:15 -0400

    Redirect process output to console.

--- 377-lab1/src/mysh/Mysh.java
+++ 377-lab1/src/mysh/Mysh.java
@@ -1,19 +1,36 @@
+
 package mysh;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Mysh{
 	
-	private Mysh(){}
+	public static void prompt(){
+		System.out.print("mysh> ");
+	}
 	
-	public static void execute(String[] cmd) throws IOException, InterruptedException{
+	public static void execute(String[] cmd) throws IOException,
+			InterruptedException{
 		Process p = new ProcessBuilder(cmd).start();
+		InputStream is = p.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		String line;
+		// System.out.printf("Output of running %s is:\n",
+		// Arrays.toString(command));
+		while((line = br.readLine()) != null){
+			System.out.println(line);
+		}
 		int exitValue = p.waitFor();
 		if(exitValue == 0){
 			System.out.println("Process exited normally");
 		}else{
-			System.out.printf("Process exited abnormally. Error %d.%n", exitValue);
+			System.out.printf("Process exited abnormally. Error %d.%n",
+					exitValue);
 		}
 	}
 	
